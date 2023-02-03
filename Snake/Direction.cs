43da281c00ba1fe32961caeafb2e7083a4be35fa
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2023-02-02 22:59:14 -0300

    First setups, Direction and Position Classes

---
+++ Snake/Direction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    public class Direction : IEquatable<Direction>
+    {
+        public readonly static Direction Left = new Direction(0, -1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+
+        public int rowOffset { get; }
+        public int colOffset { get; }
+
+        private Direction(int rowOffset, int colOffset)
+        {
+            this.rowOffset = rowOffset;
+            this.colOffset = colOffset;
+        }
+
+        public Direction opposite()
+        {
+            return new Direction(-rowOffset, -colOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   rowOffset == direction.rowOffset &&
+                   colOffset == direction.colOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(rowOffset, colOffset);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ Snake/Position.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    public class Position
+    {
+        public int row { get; }
+        public int column { get; }
+
+        public Position(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public Position translate(Direction direction)
+        {
+            return new Position(row + direction.rowOffset, 
+                column + direction.colOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position &&
+                   row == position.row &&
+                   column == position.column;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(row, column);
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+    }
+}
